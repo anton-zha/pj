e1d7e5660666497b5ef2ffb6cfdd4a8f0eda1b42
anton-zha <antonzharovskiy@gmail.com>
2025-01-26 11:27:00 +0200

    change HTML, CSS and JS

--- src/main.js
+++ src/main.js
@@ -1,137 +1,53 @@
-let iconCart = document.querySelector('.iconCart');
-let cart = document.querySelector('.cart');
-let container = document.querySelector('.container');
-let close = document.querySelector('.close');
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('.body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
 
-iconCart.addEventListener('click', function(){
-    if(cart.style.right == '-100%'){
-        cart.style.right = '0';
-        container.style.transform = 'translateX(-400px)';
-    }else{
-        cart.style.right = '-100%';
-        container.style.transform = 'translateX(0)';
-    }
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
 })
-close.addEventListener('click', function (){
-    cart.style.right = '-100%';
-    container.style.transform = 'translateX(0)';
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
 })
-
-
-let products = null;
-// get data from file json
-fetch('product.json')
-    .then(response => response.json())
-    .then(data => {
-        products = data;
-        addDataToHTML();
-})
-
-//show datas product in list 
-function addDataToHTML(){
-    // remove datas default from HTML
-    let listProductHTML = document.querySelector('.listProduct');
-    listProductHTML.innerHTML = '';
-
-    // add new datas
-    if(products != null) // if has data
+let products = [
     {
-        products.forEach(product => {
-            let newProduct = document.createElement('div');
-            newProduct.classList.add('item');
-            newProduct.innerHTML = 
-            `<img src="${product.image}" alt="">
-            <h2>${product.name}</h2>
-            <div class="price">$${product.price}</div>
-            <button onclick="addCart(${product.id})">Add To Cart</button>`;
-
-            listProductHTML.appendChild(newProduct);
-
-        });
-    }
-}
-//use cookie so the cart doesn't get lost on refresh page
-
-
-let listCart = [];
-function checkCart(){
-    var cookieValue = document.cookie
-    .split('; ')
-    .find(row => row.startsWith('listCart='));
-    if(cookieValue){
-        listCart = JSON.parse(cookieValue.split('=')[1]);
-    }else{
-        listCart = [];
-    }
-}
-checkCart();
-function addCart($idProduct){
-    let productsCopy = JSON.parse(JSON.stringify(products));
-    //// If this product is not in the cart
-    if(!listCart[$idProduct]) 
+        id: 1,
+        name: 'Кава зернова 100% Арабіка “Марагоджип Гватемала”',
+        image: 'https://foodboom.com.ua/wp-content/uploads/2023/11/%D0%BA%D0%B0%D0%B2%D0%B0-1024x344.jpg',
+        price: 200
+    },
     {
-        listCart[$idProduct] = productsCopy.filter(product => product.id == $idProduct)[0];
-        listCart[$idProduct].quantity = 1;
-    }else{
-        //If this product is already in the cart.
-        //I just increased the quantity
-        listCart[$idProduct].quantity++;
-    }
-    document.cookie = "listCart=" + JSON.stringify(listCart) + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;";
-
-    addCartToHTML();
+        id: 2,
+        name: 'Кава мелена 100% Арабіка “Марагоджип Гватемала”',
+        image: 'https://foodboom.com.ua/wp-content/uploads/2023/06/kofe-molotyi-100-arabyka-marahodzhyp-hvatemalaid-49204607208506_-698d34536c-300x300.jpg',
+        price: 300
+    },
+    {
+        id: 3,
+        name: 'Кава мелена 100% Робуста “Індія”',
+        image: 'https://foodboom.com.ua/wp-content/uploads/2023/04/kofe-molotyi-100-robusta-yndyiaid-64662496827576_-31d09e91bd-300x300.jpg',
+        price: 300
+    },
+    {
+        id: 4,
+        name: 'Кава мелена 100% Арабіка “Гондурас Високогірний”',
+        image: 'https://foodboom.com.ua/wp-content/uploads/2023/04/kofe-molotyi-100-arabyka-honduras-vysokohornyiid-28363872636184_-454ff6cab9-300x300.jpg',
+        price: 200
+    },
+];
+let listCards = [];
+function initApp(){
+    products.forEach((value, key)=>{
+        let newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}"/>
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+        `;
+        list.appendChild(newDiv);
+    })
 }
-addCartToHTML();
-function addCartToHTML(){
-    // clear data default
-    let listCartHTML = document.querySelector('.listCart');
-    listCartHTML.innerHTML = '';
-
-    let totalHTML = document.querySelector('.totalQuantity');
-    let totalQuantity = 0;
-    // if has product in Cart
-    if(listCart){
-        listCart.forEach(product => {
-            if(product){
-                let newCart = document.createElement('div');
-                newCart.classList.add('item');
-                newCart.innerHTML = 
-                    `<img src="${product.image}">
-                    <div class="content">
-                        <div class="name">${product.name}</div>
-                        <div class="price">$${product.price} / 1 product</div>
-                    </div>
-                    <div class="quantity">
-                        <button onclick="changeQuantity(${product.id}, '-')">-</button>
-                        <span class="value">${product.quantity}</span>
-                        <button onclick="changeQuantity(${product.id}, '+')">+</button>
-                    </div>`;
-                listCartHTML.appendChild(newCart);
-                totalQuantity = totalQuantity + product.quantity;
-            }
-        })
-    }
-    totalHTML.innerText = totalQuantity;
-}
-function changeQuantity($idProduct, $type){
-    switch ($type) {
-        case '+':
-            listCart[$idProduct].quantity++;
-            break;
-        case '-':
-            listCart[$idProduct].quantity--;
-
-            // if quantity <= 0 then remove product in cart
-            if(listCart[$idProduct].quantity <= 0){
-                delete listCart[$idProduct];
-            }
-            break;
-    
-        default:
-            break;
-    }
-    // save new data in cookie
-    document.cookie = "listCart=" + JSON.stringify(listCart) + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;";
-    // reload html view cart
-    addCartToHTML();
-}
+initApp();
